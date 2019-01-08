bde002a3b5e876f60219727127d78de466845173
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-08 20:39:56 +0000

    now has video info and stuff

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,22 +7,36 @@ function querySt(ji) { //ORIGINAL FROM https://stackoverflow.com/a/5422360
     }
 }
 
-function initDone() { 
+function isEmpty(str) { return !str.replace(/\s+/, '').length; }
+
+
+function initDone() {
     let file = querySt('file');
-    if (file === undefined) { 
+    if (file === undefined || file === 'undefined') {
         document.getElementById('text').innerText = "404: File not found!";
         document.getElementById("link").style.display = "none";
         return;
     }
-    document.getElementById('link').setAttribute('href', `files/${file}`); 
-}   
-
-function postReq(url) {
-  document.getElementById('link').classList.add('clicked');
-  document.getElementById('link').classList.remove('link');
-  axios.get(`download?url=${url}`).then(res => {
-    window.location.href = `done.html?file=${res.file}`
-  });
+    document.getElementById('link').setAttribute('href', `files/${file}`);
 }
 
-function changeDL() { document.getElementById('link').setAttribute('onclick', `javascript:(postReq('${document.getElementById('input').value}'))`); }
+function postReq(url) {
+    document.getElementById('link').classList.add('clicked');
+    document.getElementById('link').classList.remove('link');
+    axios.get(`download?url=${url}`).then(res => { window.location.href = `done.html?file=${res.data}`; });
+}
+
+function changeDL() {
+    function isEmpty(str) { return !str.replace(/\s+/, '').length; }
+    document.getElementById('text').innerText = 'Loading...'; 
+    document.getElementById('img').src = 'assets/img/question.png';
+    if (isEmpty(document.getElementById('input').value)) {
+        document.getElementById('text').innerText = '';
+        document.getElementById('img').src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    }
+    axios.get(`info?url=${document.getElementById('input').value}`).then(res => { 
+        document.getElementById('text').innerText = res.data.title; 
+        document.getElementById('img').src = res.data.thumb; 
+    });
+    document.getElementById('link').setAttribute('onclick', `javascript:(postReq('${document.getElementById('input').value}'))`);
+}
--- routes.js
+++ routes.js
@@ -10,9 +10,26 @@ router.get('/download', (req, res) => {
     let video = youtubedl(req.query.url);
     let string = randomBytes(config.fileLength).toString('hex');
     video.pipe(createWriteStream(`public/files/${string}.webm`));
+    video.on('error', (err) => { console.log(`[AquaDL] Error: ${err}`); });
     video.on('end', () => { 
         console.log(`[AquaDL] Finished! Saved to ${string}.webm`);
         res.send(`${string}.webm`); 
+    });
+});
+
+router.get('/info', (req, res) => {
+    youtubedl.getInfo(req.query.url, (err, info) => {
+        if (err) return;
+
+        console.log('id:', info.id);
+        console.log('title:', info.title);
+        console.log('url:', info.url);
+        console.log('thumbnail:', info.thumbnail);
+        console.log('description:', info.description);
+        console.log('filename:', info._filename);
+        console.log('format id:', info.format_id);
+
+        res.json({title: info.title, desc: info.description, thumb: info.thumbnail});
     });
 });
 
