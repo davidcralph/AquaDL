88de62a8615e810de864392d3b695c249616409d
David Ralph <ohlookitsderpy@protonmail.com>
2019-02-15 13:43:57 +0000

    part 2.25

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const kirbe  = require('kirbe');
 const app    = new kirbe.Server();
 const config = require('./config.json');
+const routes = require('./routes.js');
 
-app.use(require('./routes.js'));
-app.use(kirbe.static('./public'));
+routes(app);
+kirbe.static('./public');
     
 app.listen(config.port, console.log(`[AquaDL] Listening on port ${config.port}!`));
